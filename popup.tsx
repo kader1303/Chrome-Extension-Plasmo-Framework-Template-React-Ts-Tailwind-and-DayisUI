da58fab0ea9a61bbbe91024919ab81616b8e4827
kader1303 <kdryere1314@gmail.com>
2023-03-21 15:37:45 -0400

    Initial configuration

--- popup.tsx
+++ popup.tsx
@@ -1,28 +1,13 @@
-import { useState } from "react"
-
+import { useReducer } from "react"
+ 
+import "./style.css"
+ 
 function IndexPopup() {
-  const [data, setData] = useState("")
-
+  const [count, increase] = useReducer((c) => c + 1, 0)
+ 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        padding: 16
-      }}>
-      <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
-    </div>
+    <button type="button" className="btn">Hello daisyUI</button>
   )
 }
-
+ 
 export default IndexPopup
